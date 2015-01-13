f4179d1507c2c22b802e2a0ba38b6003ed06aaee
Richard Tan <chardos@gmail.com>
2015-01-13 23:02:52 +1100

    Added suffix check in acronym expander (ruby)

--- 193_easy_acronym_expander/ruby.rb
+++ 193_easy_acronym_expander/ruby.rb
@@ -1,17 +1,15 @@
-require 'ostruct'
-
-$acronyms=OpenStruct.new(
+$acronyms={
   lol: 'laugh out loud',
   dw: "don't worry",
   hf: 'have fun',
   gg: 'good game',
   brb: 'be right back',
-  g2g: 'got to go',
-  wtf: 'what the fuck',
+  gtg: 'got to go',
+  wth: 'what the hell',
   wp: 'well played',
   gl: 'good luck',
   imo: 'in my opinion'
-)
+}
 
 class Sentence
   attr_accessor :sentence
@@ -20,21 +18,38 @@ class Sentence
     @sentence = sentence
   end
 
+  def check_for_suffix(a,k)
+    without_last_letter = a[0..-2]
+    last_letter = a.chars.last(1).join
+
+    if a == k.to_s
+      $acronyms[a.to_sym]
+    elsif without_last_letter == k
+      $acronyms[a.to_sym] + last_letter
+    else 
+      false
+    end
+  end
+
   def expand
     arr = @sentence.split(' ')
-    arr.each_with_index{ |x, i| 
-      #puts x
-      $acronyms.to_h.each{ |k,v| 
-        if x == k.to_s
-          arr[i] = v
+    arr.each_with_index{ |a, i| 
+      $acronyms.each do |k, v|
+        if check_for_suffix(a, k) != false
+          arr[i] = check_for_suffix(a, k)
         end
-      }
+      end
     }
     puts arr.join(' ')
   end
 end
 
-s1 = Sentence.new('hey lol wtf is that')
+s1 = Sentence.new('hey lol, wth is that')
+s2 = Sentence.new('brb, i gtg! wp imo')
 
 s1.expand
+s2.expand
+
+
+
 
