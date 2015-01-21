f23168b48775953774053060a0b0cda3fae8004c
Richard Tan <chardos@gmail.com>
2015-01-22 00:23:32 +1100

    191: more find path

--- 191_intermediate_space_probe/find_path.rb
+++ 191_intermediate_space_probe/find_path.rb
@@ -1,6 +1,10 @@
 module Find_Path
   def find_path
     cur_pos = @start_point
+    lowest_f_score = nil
+    lowest_f_square = nil
+    closed_list = nil
+    open_list = nil
 
     #analyze adjacent blocks
     surrounding_squares(cur_pos[0], cur_pos[1]).each do |adj_pos|
@@ -17,10 +21,16 @@ module Find_Path
       f_score = g_score + h_score
       @f_score_map[adj_pos[0]][adj_pos[1]] = f_score
 
+      if lowest_f_score == nil || f_score < lowest_f_score
+        lowest_f_score = f_score
+        lowest_f_square = adj_pos
+      end
+      
+      puts "lowest #{lowest_f_square}"
 
       #if diagonal add 14 - else add 10
     end
-    
+
     #pick block with lowest F
 
   end
